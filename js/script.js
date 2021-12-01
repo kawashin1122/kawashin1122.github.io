d70b7aa797e4e3d8f57cdf9d48dd783b91fcfa0f
kawashin1122 <bbc.1122.2chan@gmail.com>
2021-12-01 15:49:17 +0900

    ECサイトデモ

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//スマホの時だけ電話番号有効化
+
+if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+  $(function() {
+    $('.tel').each(function() {
+      var str = $(this).html();
+      if ($(this).children().is('img')) {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).children().attr('alt').replace(/-/g, '')).append(str + '</a>'));
+      } else {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+      }
+    });
+  });
+}
+
+//スムーススクロール
+
+$(function(){
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+});
